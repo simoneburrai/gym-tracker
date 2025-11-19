c971e043b1fcf7e87b79950914afb3a4c48efcab
simoneburrai <burrai.s98@gmail.com>
2025-11-19 16:58:40 +0100

    feat: creatine useRoutine Hook

---
+++ src/hooks/useRoutines.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+
+const [routines, setRoutines] = useState(()=>{
+    const localRoutines = JSON.parse(localStorage.getItem("routines"));
+    if(!localRoutines){
+        localRoutines = []
+    }
+    return localRoutines
+})
+
+useEffect(()=>{
+    localStorage.setItem("routines", JSON.stringify(routines));
+}, [routines])
+
+const createRoutine = (title)=>{
+    const currentIds = routines.map(r=> r.id);
+    const biggestId = Math.max(0, ...currentIds);
+    const newRoutine = {
+        title: title,
+        id: biggestId + 1
+    };
+
+    setRoutines(routines=>[...routines, newRoutine]);
+}
+
+const deleteRoutine = (id)=>{
+    setRoutines(routines => routines.filter(r=>r.id !== Number(id)));
+}
+
+const modifyRoutine = (id, title)=>{
+    const currentRoutine = routines.find(r => r.id === Number(id));
+    currentRoutine = {
+        ...currentRoutine,
+        title: title
+    }
+}
+
+export {
+    routines, 
+    createRoutine,
+    modifyRoutine,
+    deleteRoutine
+}
